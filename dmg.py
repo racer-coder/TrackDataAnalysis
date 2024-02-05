29e0cbac7a2bfa8da65c3dbdcec00ff9999b3b08
Scott Smith <scott.smith@gmail.com>
2024-02-05 14:07:49 -0800

    Work on building a DMG for Mac

---
+++ dmg.py
@@ -0,0 +1,7 @@
+# input file to dmgbuild
+
+filename = 'dist/TrackDataAnalysis-0.1.2.dev.dmg'
+volume_name = 'TrackDataAnalysis'
+files = [('dist/TrackDataAnalysis.app', 'TrackDataAnalysis.app')]
+format = 'ULFO'
+filesystem = 'APFS'
