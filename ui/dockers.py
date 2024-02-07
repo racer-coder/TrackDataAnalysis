9024975cd24cc84aae55540e30369625c9ac51b2
Scott Smith <scott.smith@gmail.com>
2024-02-07 15:44:16 -0800

    Push presenter into a subclass to get ready for divider entries

--- ui/dockers.py
+++ ui/dockers.py
@@ -123,6 +123,51 @@ class TempDockWidget(QDockWidget):
                 if d != self and d.isFloating() and not d.isHidden():
                     d.hide()
 
+@dataclass
+class DataModelLap:
+    lap: state.LapRef
+    best_lap: int
+
+    def present(self, index, model):
+        col = index.column()
+        if col == 0:
+            return (model.right_style, str(self.lap.num))
+        if col == 1:
+            return (model.right_style, state.format_time(self.lap.duration()))
+        if col == 2:
+            return (([state.lap_colors[0]] + model.center_style[1:], '\u278a')
+                    if self.lap == model.data_view.ref_lap else (model.center_style, '\u2d54'))
+        if col == 3:
+            return (([state.lap_colors[1]] + model.center_style[1:], '\u278b')
+                    if self.lap == model.data_view.alt_lap else (model.center_style, '\u2d54'))
+        if col == 4:
+            for idx, (lap, color) in enumerate(model.data_view.extra_laps):
+                if lap == self.lap:
+                    return ([color] + model.center_style[1:], chr(0x278c + idx))
+            return (model.center_style, '\u2d54')
+
+        if col == 5:
+            return (model.right_style,
+                    state.format_time(self.lap.duration() - self.best_lap))
+        if col == 6:
+            return (model.right_style,
+                    state.format_time(self.lap.offset.time) if self.lap.offset.time else None)
+        if col == 7:
+            return (model.right_style,
+                    '%.2f' % self.lap.offset.dist if self.lap.offset.dist else None)
+        return None
+
+@dataclass
+class DataModelSection:
+    text: str
+    lap: state.LapRef = None
+
+    def present(self, index, model):
+        col = index.column()
+        if col == 0:
+            return (model.section_style, self.text)
+        return (model.section_style, None)
+
 class DataDockModel(FastTableModel):
     headings = ['Lap', 'Time', 'R', 'A', '3', 'Delta', 'Time off', 'Dist off']
 
@@ -132,37 +177,19 @@ class DataDockModel(FastTableModel):
         self.laps = []
         self.right_style = [QtGui.QPen(QtGui.QColor(192, 192, 192)), None, None, Qt.AlignRight]
         self.center_style = self.right_style[:3] + [Qt.AlignHCenter]
+        self.section_style = [QtGui.QPen(QtGui.QColor(255, 255, 255)), QtGui.QColor(64, 64, 64),
+                              None, Qt.AlignLeft]
 
-    def set_data(self, laps, font):
+    def set_data(self, laps, font, section_font):
         old_len = len(self.laps)
         self.laps = laps
         self.right_style[2] = font
         self.center_style[2] = font
+        self.section_style[2] = section_font
         super().set_data(self.headings, len(laps))
 
     def present(self, index):
-        lapref, best_lap = self.laps[index.row()]
-        col = index.column()
-        if col == 0: return (self.right_style, str(lapref.num))
-        if col == 1: return (self.right_style, state.format_time(lapref.duration()))
-        if col == 2:
-            return (([state.lap_colors[0]] + self.center_style[1:], '\u278a')
-                    if lapref == self.data_view.ref_lap else (self.center_style, '\u2d54'))
-        if col == 3:
-            return (([state.lap_colors[1]] + self.center_style[1:], '\u278b')
-                    if lapref == self.data_view.alt_lap else (self.center_style, '\u2d54'))
-        if col == 4:
-            for idx, (lap, color) in enumerate(self.data_view.extra_laps):
-                if lap == lapref:
-                    return ([color] + self.center_style[1:], chr(0x278c + idx))
-            return (self.center_style, '\u2d54')
-
-        if col == 5: return (self.right_style, state.format_time(lapref.duration() - best_lap))
-        if col == 6: return (self.right_style,
-                             state.format_time(lapref.offset.time) if lapref.offset.time else '')
-        if col == 7: return (self.right_style,
-                             '%.2f' % lapref.offset.dist if lapref.offset.dist else '')
-        return None
+        return self.laps[index.row()].present(index, self)
 
 class DataDockWidget(TempDockWidget):
     def __init__(self, mainwindow, toolbar):
@@ -202,7 +229,8 @@ class DataDockWidget(TempDockWidget):
     def clickCell(self, index):
         row = index.row()
         col = index.column()
-        lapref = self.model.laps[row][0]
+        lapref = self.model.laps[row].lap
+        if not lapref: return # must be a section divider
         data_view = self.mainwindow.data_view
         if col <= 2:
             data_view.ref_lap = lapref
@@ -233,10 +261,28 @@ class DataDockWidget(TempDockWidget):
         font_size = 12
         font = QtGui.QFont('Tahoma')
         font.setPixelSize(widgets.devicePointScale(self, font_size))
+        section_font = QtGui.QFont(font)
+        section_font.setBold(True)
         metrics = QtGui.QFontMetrics(font)
         logs = [(logref, self.best_lap(logref)) for logref in self.mainwindow.data_view.log_files]
-        laps = [(lap, best_lap) for logref, best_lap in logs for lap in logref.laps]
-        self.model.set_data(laps, font)
+        self.table.clearSpans()
+        laps = []
+        last_date = None
+        logs.sort(key=lambda r: (r[0].log.get_metadata()['Log Date'],
+                                 r[0].log.get_metadata()['Log Time']))
+        for logref, best_lap in logs:
+            metadata = logref.log.get_metadata()
+            date = metadata['Log Date']
+            if date != last_date:
+                self.table.setSpan(len(laps), 0, 1, 8)
+                laps.append(DataModelSection('\u25bc ' + date))
+            self.table.setSpan(len(laps), 0, 1, 8)
+            laps.append(DataModelSection('  \u25bc %s' % ', '.join(
+                filter(bool, [metadata['Log Time'],
+                              metadata.get('Session', None),
+                              metadata.get('Driver', None)]))))
+            laps += [DataModelLap(lap, best_lap) for lap in logref.laps]
+        self.model.set_data(laps, font, section_font)
         self.deleg.metrics = metrics
         self.table.setColumnWidth(0, self.margin * 2 + metrics.horizontalAdvance('Lap 888'))
         self.table.setColumnWidth(1, self.margin * 2 + metrics.horizontalAdvance('88:88.888'))
