e943917150740b6713a3afe83e79d2a3104b8443
Scott Smith <scott.smith@gmail.com>
2024-06-19 16:28:19 -0700

    AIM_XRK: Don't parse laps if we have valid GPS.  And if we do parse laps, deal with a missing lap more gracefully.

--- data/aim_xrk.pyx
+++ data/aim_xrk.pyx
@@ -589,32 +589,45 @@ def _decode_gps(msg_by_type, time_offset):
                     timecodes=timecodes, sampledata=memoryview(gpsconv.alt))]
 
 def _get_laps(lat_ch, lon_ch, msg_by_type, time_offset, last_time):
+    if lat_ch and lon_ch:
+        # If we have GPS, do gps lap insert.
+
+        track = msg_by_type[_tokdec('TRK')][-1].content
+        XYZ = np.column_stack(gps.lla2ecef(np.array(lat_ch.sampledata),
+                                           np.array(lon_ch.sampledata), 0))
+        lap_markers = gps.find_laps(XYZ,
+                                    np.array(lat_ch.timecodes),
+                                    (track['sf_lat'], track['sf_long']))
+
+        lap_markers = [0] + lap_markers + [last_time - time_offset]
+
+        return [base.Lap(lap, start_time, end_time)
+                for lap, (start_time, end_time) in enumerate(
+                        zip(lap_markers[:-1], lap_markers[1:]))]
+
+    # otherwise, use the lap data provided.
     ret = []
     if _tokdec('LAP') in msg_by_type:
         for m in msg_by_type[_tokdec('LAP')]:
             # 2nd byte is segment #, see M4GT4
             segment, lap, duration, end_time = struct.unpack('xBHIxxxxxxxxI', m.content)
             end_time -= time_offset
-            if (not ret or ret[-1].num != lap) and segment == 0:
-                assert not ret or ret[-1].num + 1 == lap # deal with missing data later
-                ret.append(base.Lap(lap, end_time - duration, end_time))
-    if not lat_ch or not lon_ch:
-        # If we don't have GPS data, just return whatever laps we found.
-        return ret
-    # otherwise, we do gps lap insert.
-
-    track = msg_by_type[_tokdec('TRK')][-1].content
-    XYZ = np.column_stack(gps.lla2ecef(np.array(lat_ch.sampledata),
-                                       np.array(lon_ch.sampledata), 0))
-    lap_markers = gps.find_laps(XYZ,
-                                np.array(lat_ch.timecodes),
-                                (track['sf_lat'], track['sf_long']))
-
-    lap_markers = [0] + lap_markers + [last_time - time_offset]
-
-    return [base.Lap(lap, start_time, end_time)
-            for lap, (start_time, end_time) in enumerate(zip(lap_markers[:-1],
-                                                             lap_markers[1:]))]
+            if segment:
+                continue
+            elif not ret:
+                pass
+            elif ret[-1].num == lap:
+                continue
+            elif ret[-1].num + 1 == lap:
+                pass
+            elif ret[-1].num + 2 == lap:
+                # emit inferred lap
+                ret.append(base.Lap(lap - 1, ret[-1].end_time,
+                                    end_time - duration))
+            else:
+                assert False, 'Lap gap from %d to %d' % (ret[-1].num, lap)
+            ret.append(base.Lap(lap, end_time - duration, end_time))
+    return ret
 
 
 def AIMXRK(fname, progress):
